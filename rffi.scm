6464ccd7f81699bcb7aceb02ddc2963b302e740a
yagi <hiro.yagi0245@gmail.com>
2019-07-09 04:07:59 +0900

    eval

--- rffi.scm
+++ rffi.scm
@@ -20,23 +20,23 @@ RInside rinstance(0, NULL, false, false, true);
 
 (define (r-apply fname lst)
   (assert (and 'r-apply (list? lst)))
-  (receive (value err)
-      ((foreign-primitive void ((c-string fname) (scheme-object rsxp_list))
-         "
-Rcpp::Function docall(\"do.call\");
-Rcpp::Function f(fname);
-Rcpp::List args = Rcpp::List::create();
-while(!C_truep(C_i_nullp(rsxp_list))) {
-    Rcpp::RObject a = (SEXP)(C_c_pointer_or_null(C_u_i_car(rsxp_list)));
-    args.push_back(a);
-    rsxp_list = C_u_i_cdr(rsxp_list);
-}
-
+  (let ([lst (map s->r lst)])
+    (receive (value err)
+        ((foreign-primitive void ((c-string fname) (scheme-object rsxp_list))
+           "
 C_word *pointer = C_alloc(C_SIZEOF_POINTER);
 SEXP value;
 int error = 0;
 try {
-   value = docall(f, args);
+    Rcpp::Function docall(\"do.call\");
+    Rcpp::Function f(fname);
+    Rcpp::List args = Rcpp::List::create();
+    while(!C_truep(C_i_nullp(rsxp_list))) {
+        Rcpp::RObject a = (SEXP)(C_c_pointer_or_null(C_u_i_car(rsxp_list)));
+        args.push_back(a);
+        rsxp_list = C_u_i_cdr(rsxp_list);
+    }
+    value = docall(f, args);
 } catch(...) {
    error = -1;
 }
@@ -47,9 +47,18 @@ av[2] = C_mpointer(&pointer, value);
 av[3] = C_fix(error);
 C_values(4, av);
 ") fname lst)
-    (if (zero? err)
-        value
-        (error "r error - r-apply"))))
+      (if (zero? err)
+          value
+          (error "r error - r-apply")))))
+
+(define (r-eval expression)
+  (cond [(pair? expression)
+         (r-apply (symbol->string (car expression))
+                  (map r-eval (cdr expression)))]
+        [(symbol? expression)
+         (r-eval-string
+          (symbol->string expression))]
+        [else (s->r expression)]))
 
 (define (r-eval-string str)
   (receive (value err)
@@ -106,79 +115,62 @@ C_values(4, av);
                        (99 . FUNSXP     ) ;; /* Closure or Builtin */
                        )))
 
-(define +r-object->scheme-object-table+ (make-hash-table))
-(define +scheme-object->r-object-table+ (make-hash-table))
+(define +r->s-table+ (make-hash-table))
+(define +s->r-table+ (make-hash-table))
 
 (define (r-lambda funname)
   (lambda args
-    (rffi_apply funname args)))
+    (r-apply funname args)))
 
 (define (r-object-sexp-type rsxp)
   (hash-table-ref +sexp-type-table+ (rffi_sexp_type rsxp)))
 
 ;; Scheme -> R
-(define (scheme-object->r-object value)
-  (cond [(integer? value) value]
+(define (s->r value)
+  (cond [(integer? value) (s->r
+                           (s32vector value))]
+        [(number? value) (s->r
+                          (f64vector value))]
+        [(f64vector? value)
+         (rffi_numeric_vector value)]
+        [(s32vector? value)
+         (rffi_integer_vector value)]
+        [(list? value)
+         (r-apply "list" value)]
+        [(vector? value)
+         (r-apply "c" value)]
         [else value]))
 
 ;; R -> Scheme
-(define (r-object->scheme-object x)
+(define (r->s x)
   (let* ([type (r-object-sexp-type x)]
-         [conv (hash-table-ref/default +r-object->scheme-object-table+ type identity)])
-    (list type (conv x))))
+         [conv (hash-table-ref/default +r->s-table+ type
+                                       (lambda (x)
+                                         (cons type x)))])
+    (conv x)))
 
-(bind-type rffi_sexp c-pointer
-           ;; scheme-object->r-object r-object->scheme-object
-           )
-
+(bind-type rffi_sexp c-pointer)
 (bind*
 #<<CPP
-rffi_sexp rffi_eval(const char *str) {
-    try {
-	// on heap???
-	return rinstance.parseEval(str);
-    } catch(std::exception& ex) {
-	std::cerr << "Exception caught: " << ex.what() << std::endl;
-	return R_NilValue;
-    } catch(...){
-	std::cerr << "Unknown exception caught" << std::endl;
-	return R_NilValue;
-    }
-}
-// rsxpのscheme listであるべき
-rffi_sexp rffi_apply(const char *func_name, C_word rsxp_list) {
-    Rcpp::Function docall("do.call");
-    Rcpp::Function f(func_name);
-    Rcpp::List args = Rcpp::List::create();
-    while(!C_truep(C_i_nullp(rsxp_list))) {
-	Rcpp::RObject a = (SEXP)(C_c_pointer_or_null(C_u_i_car(rsxp_list)));
-	args.push_back(a);
-	rsxp_list = C_u_i_cdr(rsxp_list);
-    }
-    return docall(f, args);
-}
-
-double numeric_vector_ref(rffi_sexp rsxp, int i) {
+double rffi_numeric_vector_ref(rffi_sexp rsxp, int i) {
     return (REAL((SEXP) rsxp))[i];
 }
 
-rffi_sexp r_numeric_vector(double * vec, ___length(vec) int len) {
-    return Rcpp::NumericVector(vec, vec + len);
-}
-
-int integer_vector_ref(rffi_sexp rsxp, int i) {
+int rffi_integer_vector_ref(rffi_sexp rsxp, int i) {
     return (INTEGER((SEXP) rsxp))[i];
 }
 
-rffi_sexp list_ref(rffi_sexp rsxp, int i) {
+rffi_sexp rffi_list_ref(rffi_sexp rsxp, int i) {
     return Rcpp::as<Rcpp::List>((SEXP) rsxp)[i];
     // return VECTOR_ELT((SEXP) rsxp, i);
 }
 
-rffi_sexp r_list_append(rffi_sexp rsxp, rffi_sexp x) {
-    std::cout << x << "\n";
-    (Rcpp::as<Rcpp::List>((SEXP) rsxp)).push_back((SEXP) x);
-    return rsxp;
+rffi_sexp rffi_numeric_vector(double * vec, ___length(vec) int len) {
+    return Rcpp::NumericVector(vec, vec + len);
+}
+
+rffi_sexp rffi_integer_vector(int * vec, ___length(vec) int len) {
+    return Rcpp::IntegerVector(vec, vec + len);
 }
 
 int rffi_sexp_type(rffi_sexp rsxp) {
@@ -192,7 +184,7 @@ int rffi_sexp_length(rffi_sexp rsxp) {
 CPP
 )
 
-(define (make-r-vector->scheme-object ref-proc len-proc)
+(define (make-r-vector->scheme-object ref-proc len-proc make-vector vector-set!)
   (lambda (rvec)
     (let ([length (len-proc rvec)])
       (if (= length 1)
@@ -202,23 +194,23 @@ CPP
               ((= i length) vector)
             (vector-set! vector i (ref-proc rvec i)))))))
 
-(hash-table-set! +r-object->scheme-object-table+
+(hash-table-set! +r->s-table+
                  'REALSXP
-                 (make-r-vector->scheme-object numeric_vector_ref rffi_sexp_length))
+                 (make-r-vector->scheme-object rffi_numeric_vector_ref rffi_sexp_length make-f64vector f64vector-set!))
 
-(hash-table-set! +r-object->scheme-object-table+
+(hash-table-set! +r->s-table+
                  'INTSXP
-                 (make-r-vector->scheme-object integer_vector_ref rffi_sexp_length))
+                 (make-r-vector->scheme-object rffi_integer_vector_ref rffi_sexp_length make-s32vector s32vector-set!))
 
-(hash-table-set! +r-object->scheme-object-table+
-                 'VECSXP
+(hash-table-set! +r->s-table+
+'VECSXP
                  (lambda (rsxp)
                    (let ([length (rffi_sexp_length rsxp)])
                      (let loop ([i 0]
                                 [lst '()])
                        (if (< i length)
-                           (let ([item (list_ref rsxp i)])
-                             (loop (+ i 1) (cons (r-object->scheme-object item) lst)))
+                           (let ([item (rffi_list_ref rsxp i)])
+                             (loop (+ i 1) (cons (r->s item) lst)))
                            (reverse lst))))))
 
 
